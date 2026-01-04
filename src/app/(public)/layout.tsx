1c623d48d12bdd7501f9c6ea991ac60303d0c67c
Kramarich <147999794+Kramarich0@users.noreply.github.com>
2026-01-04 03:49:34 +0300

    chore(ui): правки в ui (#113)
    добавлен адаптивный цвет фона на /auth также мелкие правки стилей

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,10 +1,12 @@
+import { ReactNode } from "react";
+
 import { AppFooter } from "@/widgets/AppFooter";
 import { PublicHeader } from "@/widgets/PublicHeader";
 
-export default function PublicLayout({ children }: { children: React.ReactNode }) {
+export default function PublicLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-background flex min-h-screen flex-col">
-      <div className="bg-background sticky top-0 z-50 w-full shrink-0 border-b">
+    <div className="flex min-h-screen flex-col">
+      <div className="sticky top-0 z-50 w-full shrink-0 border-b">
         <PublicHeader />
       </div>
       <main className="flex w-full flex-1 flex-col">{children}</main>
--- src/widgets/PublicHeader/ui/PublicHeader.tsx
+++ src/widgets/PublicHeader/ui/PublicHeader.tsx
@@ -1,16 +1,30 @@
-"use client";
+import Link from "next/link";
 
+import { Button } from "@/shared/ui/button";
 import { Logo } from "@/shared/ui/Logo";
 import { ThemeToggle } from "@/shared/ui/ThemeToggle";
+import { menu } from "@/widgets/PublicHeader/model/menu";
 
 export function PublicHeader() {
   return (
-    <header className="flex h-full items-center justify-between p-4">
+    <header className="flex h-full flex-wrap items-center justify-between p-4">
       <div className="flex items-center gap-2">
         <Logo className="w-20" />
       </div>
-
+      <nav className="flex gap-4">
+        {menu.map((item) => (
+          <Button key={item.href} variant="ghost" asChild>
+            <Link href={item.href} className="flex items-center gap-2">
+              {item.icon && <item.icon />}
+              {item.title}
+            </Link>
+          </Button>
+        ))}
+      </nav>
       <div className="flex items-center gap-4">
+        <Button variant="outline" asChild>
+          <Link href="/auth">Начать</Link>
+        </Button>
         <ThemeToggle className="text-muted-foreground" />
       </div>
     </header>
